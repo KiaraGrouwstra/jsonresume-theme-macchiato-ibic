ccd1c2172a4839f789ea01db335a916c6a5d53c4
Kiara Grouwstra <kiara@bij1.org>
2024-01-05 01:20:01 +0100

    feat: allow using markdown

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const handlebarsWax = require('handlebars-wax');
 // const addressFormat = require('address-format');
 const moment = require('moment');
 const Swag = require('swag');
+const _ = require('underscore');
+const markdown = require('markdown-it')({
+  breaks: true,
+}).use(require('markdown-it-abbr'));
 
 try {
   window.Swag.registerHelpers(handlebars);
@@ -21,6 +25,13 @@ handlebars.registerHelper({
   // lowercase: s => s.toLowerCas(),
 });
 
+function convertMarkdown(str) {
+  if (str != null) {
+    return markdown.render(str);
+  }
+  return null;
+}
+
 function render(resume) {
   const dir = `${__dirname}/src`;
   const css = fs.readFileSync(`${dir}/style.css`, 'utf-8');
@@ -29,6 +40,35 @@ function render(resume) {
   const Handlebars = handlebarsWax(handlebars);
 
   Handlebars.partials(`${dir}/partials/**/*.{hbs,js}`);
+
+  resume.basics.summary = convertMarkdown(resume.basics.summary);
+  _(resume.projects).forEach(projectInfo => {
+    projectInfo.description = convertMarkdown(projectInfo.description);
+    projectInfo.highlights = _(projectInfo.highlights).map(convertMarkdown);
+  });
+  _(resume.work).forEach(workInfo => {
+    workInfo.summary = convertMarkdown(workInfo.summary);
+    workInfo.highlights = _(workInfo.highlights).map(convertMarkdown);
+  });
+  _(resume.skills).forEach(skillInfo => {
+    skillInfo.keywords = _(skillInfo.keywords).map(convertMarkdown);
+  });
+  _(resume.education).forEach(educationInfo => {
+    educationInfo.courses = _(educationInfo.courses).map(convertMarkdown);
+  });
+  _(resume.awards).forEach(a => {
+    a.summary = convertMarkdown(a.summary);
+  });
+  _(resume.volunteer).forEach(v => {
+    v.summary = convertMarkdown(v.summary);
+    v.highlights = _(v.highlights).map(convertMarkdown);
+  });
+  _(resume.publications).forEach(p => {
+    p.summary = convertMarkdown(p.summary);
+  });
+  _(resume.references).forEach(r => {
+    r.reference = convertMarkdown(r.reference);
+  });
 
   return Handlebars.compile(resumeTemplate)({
     style: `<style>${css}</style>`,
